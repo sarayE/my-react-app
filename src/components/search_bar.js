c39aaf4da5d1361f280494e36c7f6c84416980ab
Saray <Saray@SARAY>
2018-01-27 16:43:42 +0200

    Difrent between component-class and ES6-class

---
+++ src/components/search_bar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//ES6 class:
+class SearchBar extends React.Component{ //extends React.Component => Get all the functionality of component-class 
+ render(){
+     return <inpute/>;
+ }
+}
+
+
+//Difrent between component-class and ES6-class:
+//component-class : must return(JSX);
+//ES6-class: musr have a render(){return(JSX)}
+
+
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react'; //render the component
+import React from 'react'; //React.createElement to create the component
 import ReactDOM from 'react-dom'; //and insert it to the DOM
+
+import SearchBar from '../src/components/search_bar';
 
 const API_KEY = 'AIzaSyDyVUQGj031McFwo76MhUtIG0W2ZED58cA';
 
-//Create a new component/class:
+//Create a new component-class:
 const App = () => {
-  return <div>Hi!</div>; //JSX
+return (
+    <div>
+      <SearchBar/>
+    </div>
+  );
 }
 
-//'App' = class
-//'<App></App>' = instance
+//'App' = component-class
+//'<App></App>' = instance of component-class
 
 //Put the instance of the component into the DOM:
 ReactDOM.render(<App/>, document.querySelector('.container'));
