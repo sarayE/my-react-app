07b0fe0b718d29e7de574a5b1b112f62e74b44b7
Saray <Saray@SARAY>
2018-01-27 17:41:23 +0200

    ctor func and render -> work with state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
 
-//ES6 class:
+//ES6-class:
 class SearchBar extends Component{ //extends React.Component => Get all the functionality of component-class 
     
     constructor(props) {
         super(props);
 
-        this.state = { term: '' };
+        this.state = { term: '' };//just in the ctor i can write: 'this.state={}', else should i write: 'this.setState({})'
     }
     
     render() {
-        return <input onChange={(event) => console.log(event.target.value)}/>;
+        //this.state.term = event.target.value  BAD PRACTICE!!!!
+        return (
+            <div>
+                <input onChange={event => this.setState( { term: event.target.value } )}/>
+                Value of the input: {this.state.term}
+            </div>
+        );
     }
 }
 
