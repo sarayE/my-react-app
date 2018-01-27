481b6ab153fe00217d87385e69c6d95f6354cf55
Saray <Saray@SARAY>
2018-01-27 17:29:22 +0200

    Made a State

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 
 //ES6 class:
 class SearchBar extends Component{ //extends React.Component => Get all the functionality of component-class 
+    
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+    
     render() {
         return <input onChange={(event) => console.log(event.target.value)}/>;
     }
@@ -10,7 +17,8 @@ class SearchBar extends Component{ //extends React.Component => Get all the func
 
 //Difrent between component-class and ES6-class:
 //component-class : must return(JSX);
-//ES6-class: musr have a render(){return(JSX)} - use when i want get input from the user
+//ES6-class: 1.must have a render(){return(JSX)} 
+//           2.Have a stage/constructor! 
 
 
 export default SearchBar;
