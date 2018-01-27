98b4912fe031f3f462773f3b7c26c5b3c0ff8888
Saray <Saray@SARAY>
2018-01-27 17:02:37 +0200

    onChange -> event.target.value

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,16 +2,19 @@ import React, {Component} from 'react';
 
 //ES6 class:
 class SearchBar extends Component{ //extends React.Component => Get all the functionality of component-class 
- render(){
-     return <input/>;
- }
+    render() {
+        return <input onChange={this.onInputChange}/>;
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value)
+    }
 }
 
 
 //Difrent between component-class and ES6-class:
 //component-class : must return(JSX);
-//ES6-class: musr have a render(){return(JSX)}
-
+//ES6-class: musr have a render(){return(JSX)} - use when i want get input from the user
 
 
 export default SearchBar;
