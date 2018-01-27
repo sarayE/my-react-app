9105918da53fc662457aa8bd9b28c7d5a146f8ff
Saray <Saray@SARAY>
2018-01-27 14:37:38 +0200

    New way to write a function in ES6

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react'; //render the component
 import ReactDOM from 'react-dom'; //and insert it to the DOM
 
 //Create a new component/class:
+// const App = function() {
+//   return <div>Hi!</div>; //JSX
+// }
+//NEW WAY TO WRITE A FUNCTION IN ES6:
 const App = () => {
   return <div>Hi!</div>; //JSX
 }
