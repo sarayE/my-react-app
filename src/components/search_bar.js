72f14873ede4acdc15b86535d38da7ede7364308
Saray <Saray@SARAY>
2018-01-27 16:45:29 +0200

    shortcut for 'React.Component' in the ES6-class extends

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 //ES6 class:
-class SearchBar extends React.Component{ //extends React.Component => Get all the functionality of component-class 
+class SearchBar extends Component{ //extends React.Component => Get all the functionality of component-class 
  render(){
      return <inpute/>;
  }
