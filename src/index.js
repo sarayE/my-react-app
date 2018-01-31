5249b70c4ba8d7db8f2de42cd0d95acb722f722e
Saray <Saray@test.com>
2018-01-31 17:43:42 +0200

    Change the App to Es6-class and build a constructor. Also update the state in the data.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
-import React from 'react'; //React.createElement to create the component
+import React, {Component} from 'react'; //React.createElement to create the component
 import ReactDOM from 'react-dom'; //and insert it to the DOM
 import YTSearch from 'youtube-api-search';
 import SearchBar from '../src/components/search_bar';
 
 const API_KEY = 'AIzaSyAZhexzN9kifX3HlaNU5AHXE2Utok8N6wg';
 
-YTSearch({key: API_KEY, term: 'golden'}, function(data) {
-  console.log(data);
-}); 
-
-//Create a new component-class:
-const App = () => {
-return (
-    <div>
-      <SearchBar/>
-    </div>
-  );
+class App extends Component {
+	
+	constructor(props){
+		super(props);
+		
+		this.state = { videos: [] };
+		
+		//search function
+		YTSearch({key: API_KEY, term: 'golden'}, (data) => {
+			console.log(data);
+			this.setState({videos: data});
+		}); 
+	}
+		
+	render() {
+		return (
+			<div>
+			  <SearchBar/>
+			</div>
+		);
+	}
 }
 
 //'App' = component-class
 //'<App></App>' = instance of component-class
 
-//Put the instance of the component into the DOM:
+
 ReactDOM.render(<App/>, document.querySelector('.container'));
